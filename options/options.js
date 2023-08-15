861bd3c7fe7da4de81b8f14da8c79b4f2d3fa821
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2023-08-15 17:13:34 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "travel-ar-app-42706",
-  url: "https://travel-ar-app-42706.botics.co",
-  api: "https://travel-ar-app-42706.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "travel-ar-app-42706",
+  "url": "https://travel-ar-app-42706.botics.co",
+  "api": "https://travel-ar-app-42706.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI3"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
